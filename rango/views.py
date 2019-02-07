a431794cc698e0fb643c5358b0dba7a2942fd5d6
Stewart <2313225b@student.gla.ac.uk>
2019-02-07 12:43:56 +0000

    Almost finished chapter 7

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,11 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+## Import the form stuff (chapter 7)
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+
+
 def index(request):
         # Query the database for a list of the catagories stored
         # ordered by number of likes, decending order
@@ -43,5 +48,45 @@ def show_category(request, category_name_slug):
                 context_dict['pages'] = None
 
         return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	
+	form = CategoryForm()
+
+	#A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+	
+	# Have we been provided with a valid form?
+	if form.is_valid():
+		# Save the new category to the database.
+		form.save(commit=True)
+		return index(request)
+	
+	else:
+		print(form.errors)
+		return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+        try:
+                category = Category.objects.get(slug=category_name_slug)
+        except Category.DoesNotExist:
+                category = None
+
+        form = PageForm()
+        if request.method == 'POST':
+                form = PageForm(request.POST)
+                if form.is_valid():
+                        if category:
+                                page = form.save(commit=False)
+                                page.category = category
+                                page.views = 0
+                                page.save()
+                                return show_category(request, category_name_slug)
+                else:
+                        print(form.errors)
+
+        context_dict = {'form':form, 'category': category}
+        return render(request, 'rango/add_page.html', context_dict)
 
         
